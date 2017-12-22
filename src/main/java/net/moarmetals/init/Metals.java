5a00ed6d2c4877b28a0aca73b064288247b8c305
XFireEyeZ <h.filip.holmberg@gmail.com>
2017-12-22 18:26:43 +0100

    Armor?
    Added armor.... I think....

--- src/main/java/net/moarmetals/init/Metals.java
+++ src/main/java/net/moarmetals/init/Metals.java
@@ -12,38 +12,55 @@ import net.moarmetals.metal.Metal;
 
 public class Metals {
 	
-	public static Metal lithium = new Metal("lithium", 3, 1274, 8.0F, 3.0F, 10);
+	public static Metal lithium = new Metal("lithium", 3, 1274, 8.0F, 3.0F, 10, 30, new int[] {3, 6, 8, 3}, 2.0F);
 //	public static Metal francium = new Metal("francium");
 	
 	
 	public static void createMetals() {
-		createMetal(lithium.getBlock(), lithium.getIngot(), lithium.getNugget(), lithium.getSword(), lithium.getAxe(), lithium.getPickaxe(), lithium.getSpade(), lithium.getHoe());
+		createMetal(lithium.getBlock(), lithium.getOre(), lithium.getIngot(), lithium.getNugget(), lithium.getSword(), lithium.getAxe(), lithium.getPickaxe(), lithium.getSpade(), lithium.getHoe(), lithium.getHelmet(), lithium.getChestplate(), lithium.getLeggings(), lithium.getBoots());
 //		createMetal(francium.getBlock(), francium.getIngot(), francium.getNugget());
 		
 		
 	}
 	
-	public static void createMetal(Block block, Item ingot, Item nugget, Item sword, Item axe, Item pickaxe, Item spade, Item hoe) {
+	public static void createMetal(Block block, Block ore, Item ingot, Item nugget, Item sword, Item axe, Item pickaxe, Item spade, Item hoe, Item helmet, Item chestplate, Item leggings, Item boots) {
 		//Blocks
 		GameRegistry.register(block);
+		GameRegistry.register(ore);
+		
 		GameRegistry.register(new ItemBlock(block).setUnlocalizedName(block.getUnlocalizedName()).setRegistryName(block.getRegistryName()));
+		GameRegistry.register(new ItemBlock(ore).setUnlocalizedName(ore.getUnlocalizedName()).setRegistryName(ore.getRegistryName()));
+		
 		ModelLoader.setCustomModelResourceLocation(Item.getItemFromBlock(block), 0, new ModelResourceLocation(new ResourceLocation(Reference.MODID), "inventory"));
+		ModelLoader.setCustomModelResourceLocation(Item.getItemFromBlock(ore), 0, new ModelResourceLocation(new ResourceLocation(Reference.MODID), "inventory"));
 		
 		//Items
 		GameRegistry.register(ingot);
+		
 		GameRegistry.register(nugget);
 		GameRegistry.register(sword);
 		GameRegistry.register(axe);
 		GameRegistry.register(pickaxe);
 		GameRegistry.register(spade);
 		GameRegistry.register(hoe);
+		
+		GameRegistry.register(helmet);
+		GameRegistry.register(chestplate);
+		GameRegistry.register(leggings);
+		GameRegistry.register(boots);
+		
 		ModelLoader.setCustomModelResourceLocation(ingot, 0, new ModelResourceLocation(new ResourceLocation(Reference.MODID, ingot.getUnlocalizedName().substring(5)), "inventory"));
 		ModelLoader.setCustomModelResourceLocation(nugget, 0, new ModelResourceLocation(new ResourceLocation(Reference.MODID, nugget.getUnlocalizedName().substring(5)), "inventory"));
+		
 		ModelLoader.setCustomModelResourceLocation(sword, 0, new ModelResourceLocation(new ResourceLocation(Reference.MODID, sword.getUnlocalizedName().substring(5)), "inventory"));
 		ModelLoader.setCustomModelResourceLocation(axe, 0, new ModelResourceLocation(new ResourceLocation(Reference.MODID, axe.getUnlocalizedName().substring(5)), "inventory"));
 		ModelLoader.setCustomModelResourceLocation(pickaxe, 0, new ModelResourceLocation(new ResourceLocation(Reference.MODID, pickaxe.getUnlocalizedName().substring(5)), "inventory"));
 		ModelLoader.setCustomModelResourceLocation(spade, 0, new ModelResourceLocation(new ResourceLocation(Reference.MODID, spade.getUnlocalizedName().substring(5)), "inventory"));
 		ModelLoader.setCustomModelResourceLocation(hoe, 0, new ModelResourceLocation(new ResourceLocation(Reference.MODID, hoe.getUnlocalizedName().substring(5)), "inventory"));
 		
+		ModelLoader.setCustomModelResourceLocation(helmet, 0, new ModelResourceLocation(new ResourceLocation(Reference.MODID, helmet.getUnlocalizedName().substring(5)), "inventory"));
+		ModelLoader.setCustomModelResourceLocation(chestplate, 0, new ModelResourceLocation(new ResourceLocation(Reference.MODID, chestplate.getUnlocalizedName().substring(5)), "inventory"));
+		ModelLoader.setCustomModelResourceLocation(leggings, 0, new ModelResourceLocation(new ResourceLocation(Reference.MODID, leggings.getUnlocalizedName().substring(5)), "inventory"));
+		ModelLoader.setCustomModelResourceLocation(boots, 0, new ModelResourceLocation(new ResourceLocation(Reference.MODID, boots.getUnlocalizedName().substring(5)), "inventory"));
 	}
 }
--- src/main/java/net/moarmetals/metal/Metal.java
+++ src/main/java/net/moarmetals/metal/Metal.java
@@ -3,9 +3,12 @@ package net.moarmetals.metal;
 import net.minecraft.block.Block;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
+import net.minecraft.init.SoundEvents;
+import net.minecraft.inventory.EntityEquipmentSlot;
 import net.minecraft.item.Item;
 import net.minecraft.item.Item.ToolMaterial;
-import net.minecraft.item.ItemAxe;
+import net.minecraft.item.ItemArmor;
+import net.minecraft.item.ItemArmor.ArmorMaterial;
 import net.minecraft.item.ItemHoe;
 import net.minecraft.item.ItemPickaxe;
 import net.minecraft.item.ItemSpade;
@@ -29,38 +32,55 @@ public class Metal {
 	public static ItemSpade spade;
 	public static ItemHoe hoe;
 	
+	public static ItemArmor helmet;
+	public static ItemArmor chestplate;
+	public static ItemArmor leggings;
+	public static ItemArmor boots;
+	
 	public static String name;
 	
 	public static int harvestLevel;
 	public static int durability;
 	public static int enchantability;
+	public static int armorDurability;
+	public static int[] reductionAmounts;
 	
 	public static float efficiency;
 	public static float damage;
+	public static float toughness;
 	
-	public static final ToolMaterial material = EnumHelper.addToolMaterial(name, harvestLevel, durability, efficiency, damage, enchantability);
+	public static final ToolMaterial toolMaterial = EnumHelper.addToolMaterial(name, harvestLevel, durability, efficiency, damage, enchantability);
 	
+	public static ArmorMaterial armorMaterial = EnumHelper.addArmorMaterial(name, Reference.MODID + ":" + name, armorDurability, reductionAmounts, enchantability, SoundEvents.ITEM_ARMOR_EQUIP_IRON, toughness);
 
-	public Metal(String name, int harvestLevel, int durability, float efficiency, float damage, int enchantability) {
+	public Metal(String name, int harvestLevel, int durability, float efficiency, float damage, int enchantability, int armorDurability, int[] reductionAmounts, float toughness) {
 		ingot = new MetalIngot(name);
 		nugget = new MetalNugget(name);
 		block = new MetalBlock(name);
 		ore = new MetalOre(name);
 		
-		sword = new MetalSword(material, name);
-		axe = new MetalAxe(material, name);
-		pickaxe = new MetalPickaxe(material, name);
-		spade = new MetalSpade(material, name);
-		hoe = new MetalHoe(material, name);
+		sword = new MetalSword(toolMaterial, name);
+		axe = new MetalAxe(toolMaterial, name);
+		pickaxe = new MetalPickaxe(toolMaterial, name);
+		spade = new MetalSpade(toolMaterial, name);
+		hoe = new MetalHoe(toolMaterial, name);
+		
+		helmet = new MetalHelmet(armorMaterial, name);
+		chestplate = new MetalChestplate(armorMaterial, name);
+		leggings = new MetalLeggings(armorMaterial, name);
+		boots = new MetalBoots(armorMaterial, name);
 		
 		this.name = name;
 		
 		this.harvestLevel = harvestLevel;
 		this.durability = durability;
 		this.enchantability = enchantability;
+		this.armorDurability = armorDurability;
+		this.reductionAmounts = reductionAmounts;
 		
 		this.efficiency = efficiency;
 		this.damage = damage;
+		this.toughness = toughness;
 	}
 	
 	public static Item getIngot() {
@@ -97,6 +117,22 @@ public class Metal {
 	
 	public static Item getHoe() {
 		return hoe;
+	}
+	
+	public static Item getHelmet() {
+		return helmet;
+	}
+	
+	public static Item getChestplate() {
+		return chestplate;
+	}
+	
+	public static Item getLeggings() {
+		return leggings;
+	}
+	
+	public static Item getBoots() {
+		return boots;
 	}
 	
 	public static class MetalIngot extends Item {
@@ -200,6 +236,46 @@ public class Metal {
 			setRegistryName(new ResourceLocation(Reference.MODID, name + "_hoe"));
 			setCreativeTab(Tabs.mm);
 			
+		}
+		
+	}
+	
+	public static class MetalHelmet extends ItemArmor {
+
+		public MetalHelmet(ArmorMaterial armorMaterial, String name) {
+			super(armorMaterial, 1, EntityEquipmentSlot.HEAD);
+			setUnlocalizedName(name + "_helmet");
+			setRegistryName(new ResourceLocation(Reference.MODID, name + "_helmet"));
+		}
+		
+	}
+	
+	public static class MetalChestplate extends ItemArmor {
+
+		public MetalChestplate(ArmorMaterial armorMaterial, String name) {
+			super(armorMaterial, 1, EntityEquipmentSlot.CHEST);
+			setUnlocalizedName(name + "_chestplate");
+			setRegistryName(new ResourceLocation(Reference.MODID, name + "_chestplate"));
+		}
+		
+	}
+	
+	public static class MetalLeggings extends ItemArmor {
+
+		public MetalLeggings(ArmorMaterial armorMaterial, String name) {
+			super(armorMaterial, 2, EntityEquipmentSlot.LEGS);
+			setUnlocalizedName(name + "_leggings");
+			setRegistryName(new ResourceLocation(Reference.MODID, name + "_leggings"));
+		}
+		
+	}
+	
+	public static class MetalBoots extends ItemArmor {
+
+		public MetalBoots(ArmorMaterial armorMaterial, String name) {
+			super(armorMaterial, 1, EntityEquipmentSlot.FEET);
+			setUnlocalizedName(name + "_boots");
+			setRegistryName(new ResourceLocation(Reference.MODID, name + "_boots"));
 		}
 		
 	}
